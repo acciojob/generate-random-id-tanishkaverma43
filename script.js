0855021c2b37fc4eb3a7b9a04e3a8394fb599812
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-02 16:49:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function makeid(l) {
 	let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 	for(var i=0; i<l; i++){
-	let result += characters.charAt(Math.floor(Math.random() * characters.length));
+	 result += characters.charAt(Math.floor(Math.random() * characters.length));
 	}
 	return result;
 }
